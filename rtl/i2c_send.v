eea9fb84770c0a55d62d4a8a7809c1d0311b11fe
Core_kingdom <2972880695@qq.com>
2025-04-03 15:32:27 +0800

    i2c_send simulation done

--- rtl/i2c_send.v
+++ rtl/i2c_send.v
@@ -1,12 +1,10 @@
 module i2c_send(
     input       wire                    rst_n           ,
-    input       wire                    clk             ,// i2c clk 2x
-    
+    input       wire                    clk             ,// i2c clk 2x  
     input       wire                    pre_ready       ,// must be beased on the clk
     input       wire        [7:0]       pre_data        ,
-
-    // input       wire                    i2c_sda_i       ,
-
+    output      reg                     byte_done       ,
+    output      wire                    out_flag        ,// only for simulation
     output      wire                    i2c_scl         ,// five mode:100kHz,400kHz,1MHz,3.4MHz,5MHz
     inout       wire                    i2c_sda          // set high in ACK
 );
@@ -17,6 +15,7 @@ module i2c_send(
     reg     ack_flag        ;
     reg [3:0]   status      ;
     reg [2:0]   cnt         ;
+    reg [7:0]   data_r      ;
 
     localparam      IDLE    =   5'b0    ,
                     START   =   5'b10   ,
@@ -27,6 +26,19 @@ module i2c_send(
     assign i2c_sda   = i2c_sda_o ;
     assign i2c_sda_i = i2c_sda   ;
     assign i2c_scl = start_flag ? clk_2 & clk : 1'b1 ;// duty cycle 75%
+    
+    assign out_flag = status == ANSWER ? 1'b1:1'b0;
+
+    always@(posedge clk or negedge rst_n)begin
+        if(!rst_n)begin
+            data_r <= 8'd0   ;
+        end else begin
+            if(pre_ready)
+                data_r  <= pre_data ;
+            else
+                data_r  <= data_r   ;
+        end
+    end
 
     // hit a beat to keep the timing stable
 //    always@(posedge clk or negedge rst_n)begin
@@ -89,27 +101,32 @@ module i2c_send(
         if(!rst_n)begin
             start_flag  =   1'b0    ;
             i2c_sda_o   =   1'b1    ;
+            byte_done   =   1'b1    ;
             ack_flag    =   1'b0    ;
         end else begin
             case(status)begin
                 IDLE:begin
                     start_flag  =   1'b0    ;
                     i2c_sda_o   =   1'b1    ;
+                    byte_done   =   1'b0    ;
                     ack_flag    =   1'b0    ;
                 end
                 START:begin
                     start_flag  =   1'b1    ;
                     i2c_sda_o   =   1'b0    ;
+                    byte_done   =   1'b0    ;
                     ack_flag    =   1'b0    ;
                 end
                 SEND:begin
                     start_flag  =   1'b1    ;
                     i2c_sda_o   =   pre_data[cnt];
+                    byte_done   =   1'b0    ;
                     ack_flag    =   1'b0    ;
                 end
                 ANSWER:begin
                     start_flag  =   1'b1    ;
                     i2c_sda_o   =   1'bz    ;
+                    byte_done   =   1'b1    ;
                     if(i2c_scl && i2c_sda_i)
                         ack_flag    =   1'b1;
                     else if(~i2c_sda_i && i2c_scl)
@@ -127,9 +144,12 @@ module i2c_send(
                     end
 
                     ack_flag    =   1'b0    ;
+                    byte_done   =   1'b0    ;
                 end
             endcase
         end
     end
+    
+    
 
 endmodule
---
+++ sim/i2c_send_tb.sv
@@ -0,0 +1,54 @@
+module i2c_send_tb();
+    logic            rst_n       ;
+    logic            clk         ;
+    logic            pre_ready   ;
+    logic  [7:0]     pre_data    ;
+    logic            i2c_scl     ;
+    logic            i2c_sda     ;
+    logic            byte_done   ;
+    logic            out_flag    ;
+
+    i2c_send u_i2c_send(
+      .rst_n     (rst_n    ),        
+      .clk       (clk      ),   
+      .pre_ready (pre_ready), 
+      .pre_data  (pre_data ), 
+      .out_flag  (out_flag ),
+      .i2c_scl   (i2c_scl  ), 
+      .i2c_sda   (i2c_sda  ), 
+      .byte_done (byte_done) 
+    );
+
+    initial begin
+    clk = 1'b1;
+    forever
+        # 10 clk = ~clk ;
+    end
+
+    always@(posedge clk or negedge rst_n)begin
+        if(!rst_n)begin
+            pre_ready <= 1'b1   ;
+            pre_data  <= 8'b1111_0000;
+        end else if(byte_done)begin
+            pre_ready <= 1'b1   ;
+            pre_data  <= pre_data + 8'd1;
+        end else begin
+            pre_ready <= 1'b0   ;
+            pre_data  <= pre_data;
+        end
+    end
+
+    assign i2c_sda = out_flag ? (pre_data <= 8'b1111_0011 ? 1'b0:1'b1):1'bz;
+   
+    initial begin
+        rts_n = 0;
+        # 100 rst_n = 1;
+    end
+
+    initial begin 
+        $fsdbDumpfile("tb.fsdb");
+        $fsdbDumpvars("0,i2c_send_tb");
+        #100 $finish;
+    end
+
+endmodule
